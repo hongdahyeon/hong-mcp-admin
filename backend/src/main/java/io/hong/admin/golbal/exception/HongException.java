0f91144bf379a71503ae067ad8e9d38b8cfcad08
hongdahyeon <hyeon8287@gmail.com>
2026-03-03 01:22:55 +0900

    feat: 공통 예외 처리 및 GlobalAdvice 적용

---
+++ backend/src/main/java/io/hong/admin/golbal/exception/HongException.java
@@ -0,0 +1,35 @@
+package io.hong.admin.golbal.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * packageName    : io.hong.admin.golbal.exception
+ * fileName       : HongException
+ * author         : note
+ * date           : 2026-03-03
+ * description    : HongException Class - 공통 예외 처리
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2026-03-03        home       최초 생성
+ */
+
+@Getter
+public class HongException extends Exception {
+
+    private final HttpStatus httpStatus;
+
+    public HongException() {
+        this(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류입니다.");
+    }
+
+    public HongException(String message) {
+        this(HttpStatus.INTERNAL_SERVER_ERROR, message);
+    }
+
+    public HongException(HttpStatus httpStatus, String message) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+}
---
+++ backend/src/main/java/io/hong/admin/golbal/global/GlobalRestException.java
@@ -0,0 +1,29 @@
+package io.hong.admin.golbal.global;
+
+import io.hong.admin.golbal.exception.HongException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * packageName    : io.hong.admin.golbal.global
+ * fileName       : GlobalRestException
+ * author         : note
+ * date           : 2026-03-03
+ * description    : GlobalRestException Class
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2026-03-03        home       최초 생성
+ */
+
+@RestControllerAdvice
+public class GlobalRestException {
+
+    @ExceptionHandler(HongException.class)
+    public ResponseEntity<String> hongExeption(HongException e) {
+        return ResponseEntity
+                    .status(e.getHttpStatus())
+                    .body(e.getMessage());
+    }
+}
