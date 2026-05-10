4c5d7f0f9ae7d170a1043497b20e8b8982499b6f
hong <hyeon8287@gmail.com>
2026-05-10 15:32:54 +0900

    feat: 게시판 코드 추가, description 추가

--- backend/src/main/java/io/hong/admin/domain/board/enumcd/BoardCode.java
+++ backend/src/main/java/io/hong/admin/domain/board/enumcd/BoardCode.java
@@ -12,16 +12,22 @@ import lombok.Getter;
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2026-03-04        home       최초 생성
+ * 2026-05-10        note       게시판 코드 추가, description 추가
  */
 
 @Getter
 public enum BoardCode {
 
-    NOTICE,
-    FREE,
-    QNA,
-    FAQ;
+    NOTICE("공지사항"),
+    FREE("자유게시판"),
+    QNA("qna"),
+    FAQ("faq"),
+    TERMS("이용약관"),
+    PRIVACY("개인정보처리방침");
 
-    private BoardCode() {
+    private String description;
+
+    private BoardCode(String description) {
+        this.description = description;
     }
 }
