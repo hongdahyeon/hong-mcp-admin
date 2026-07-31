2b823ae2d4c48679781811b1b502da306f6b5d1a
hong <hyeon8287@gmail.com>
2026-08-01 06:58:27 +0900

    feat: Workshop(공방) 페이징 BE 로직 추가

--- backend/src/main/java/io/hong/admin/domain/workshop/repository/HWorkshopRepository.java
+++ backend/src/main/java/io/hong/admin/domain/workshop/repository/HWorkshopRepository.java
@@ -1,7 +1,11 @@
 package io.hong.admin.domain.workshop.repository;
 
+import io.hong.admin.domain.workshop.dto.response.WorkshopListResponse;
 import io.hong.admin.domain.workshop.entity.HWorkshop;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * packageName    : io.hong.admin.domain.workshop.repository
@@ -15,4 +19,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * 2026-03-04        home       최초 생성
  */
 public interface HWorkshopRepository extends JpaRepository<HWorkshop, Long> {
+
+    @Query(value = "" +
+            "SELECT " +
+            "new io.hong.admin.domain.workshop.dto.response.WorkshopListResponse(w) " +
+            "FROM HWorkshop w ",
+            countQuery = "SELECT count(w) FROM HWorkshop w" +
+                    "")
+    Page<WorkshopListResponse> findAllWorkshop(Pageable pageable);
 }
